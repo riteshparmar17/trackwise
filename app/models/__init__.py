49b0cb0e59681b355434eddf849620ef37acee1b
Ritesh <39963205+riteshparmar17@users.noreply.github.com>
2026-03-18 01:16:54 -0400

    Revert "Feature/expenses"

--- app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from flask import current_app, g
-
-def get_db():
-    """Return MongoDB database for this request context."""
-    if 'db' not in g:
-        client = MongoClient(current_app.config['MONGO_URI'])
-        g.db = client[current_app.config['MONGO_DBNAME']]
-    return g.db
